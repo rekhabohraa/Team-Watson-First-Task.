51987833fdf8380cfab7f0aa233fdcb658141448
grace190 <58595422+grace190@users.noreply.github.com>
2020-08-02 19:27:24 +0100

    index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+name = "Grace Ajayi"
+email = "gracejeta08@gmail.com"
+language = "python"
+biostack = "drug development"
+slack_username = "@grace"
+print(name)
+print(email)
+print(language)
+print(biostack)
+print(slack_username)
