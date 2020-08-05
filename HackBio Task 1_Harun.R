709e51aa74765c0ddf07888ee3421abce8363bf8
Harun234 <69098256+Harun234@users.noreply.github.com>
2020-08-05 07:20:55 +0100

    Add files via upload
    My first HackBio task with R programming

---
+++ HackBio Task 1_Harun.R
@@ -0,0 +1,11 @@
+# HACKBIO TASKONE ----
+Name <- c("Aremu Kolawole Harun")
+Name
+Email <- "harunaremu@gmail.com"
+Email
+Language <- "R programming"
+Language
+Biostack <- "Genomics"
+Biostack
+Slack_Username <- "@Harun"
+Slack_Username
