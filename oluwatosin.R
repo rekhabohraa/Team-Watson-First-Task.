296bc0958675759f8c1a2802b7fa1732e8dc60be
Oluwatosin <69093030+Tjesctacy@users.noreply.github.com>
2020-08-05 21:08:20 +0100

    Add files via upload

---
+++ oluwatosin.R
@@ -0,0 +1,10 @@
+Name <- c("Jegede Oluwatosin Ayodeji")
+Name
+Email <- "tjesctacy@gmail.com"
+Email
+Language <- "R programming"
+Language
+Biostack <- "Genomics"
+Biostack
+Slack_Username <- "@Oluwatosin"
+Slack_Username
